a79a8dcf23e5acb55f66650135cdcd07e1820bce
i1990jain <i1990jain@gmail.com>
2016-05-26 16:07:10 +0200

    change in page configuration, Script Loading

---
+++ src/main/webapp/WEB-INF/pages/register.html
@@ -0,0 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<title>${message}</title>
+
+<!-- Bootstrap -->
+<link href="resources/css/bootstrap.min.css" rel="stylesheet">
+<link href="resources/css/welcome.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body ng-app="wcp" class="ng-cloak">
+	<div class="generic-container" ng-controller="RegisterController">
+		<h1 align="center">${message}</h1>
+		
+	</div>
+
+	<!-- Scripts always at the end of body-->
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script
+		src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="resources/js/jars/bootstrap.min.js"></script>
+
+
+	<script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.js"></script>
+    <script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-route.js"></script>
+    <script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-resource.js"></script>
+    <script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-cookies.js"></script>
+    <script src="<c:url value='/resources/js/app.js' />"></script>
+	<script src="<c:url value='/resources/js/service/user_service.js' />"></script>
+	<script
+		src="<c:url value='/resources/js/controller/user_controller.js' />"></script>
+	
+</body>
+</html>
---
+++ src/main/webapp/resources/js/app/app.js
@@ -0,0 +1,89 @@
+//'use strict';
+
+var app = angular.module('wcp',['ngRoute','ngResource']);
+var httpHeaders, message;
+
+app.config(function ($routeProvider, $httpProvider) {
+    //configure the rounting of ng-view
+    $routeProvider
+            .when('/',
+            		{controller: 'LoginController',
+    					templateUrl: 'login.jsp',
+						publicAccess: true})
+            .when('/login',
+                    {controller: 'LoginController',
+            			templateUrl: 'login.jsp',
+                        publicAccess: true})
+            .when('/home',
+                    {controller: 'LoginController',
+                        templateUrl: 'home.jsp'});
+
+
+    //configure $http to catch message responses and show them
+    $httpProvider.interceptors.push(function ($q) {
+        var setMessage = function (response) {
+            //if the response has a text and a type property, it is a message to be shown
+            if (response.data.text && response.data.type) {
+                message = {
+                    text: response.data.text,
+                    type: response.data.type,
+                    show: true
+                };
+            }
+        };
+
+        return {
+            //this is called after each successful server request
+            'response': function (response) {
+                // console.log('request:' + response);
+                setMessage(response);
+                return response || $q.when(response);
+            },
+            //this is called after each unsuccessful server request
+            'responseError': function (response) {
+                //console.log('requestError:' + response);
+                setMessage(response);
+                return $q.reject(response);
+            }
+
+        };
+    });
+
+    $httpProvider.interceptors.push(function ($rootScope, $q) {
+
+        return {
+            'request': function (config) {
+                // console.log('request:' + config);
+                return config || $q.when(config);
+            },
+            'requestError': function (rejection) {
+                // console.log('requestError:' + rejection);
+                return rejection;
+            },
+            //success -> don't intercept
+            'response': function (response) {
+                // console.log('response:' + response);
+                return  response || $q.when(response);
+            },
+            //error -> if 401 save the request and broadcast an event
+            'responseError': function (response) {
+                console.log('responseError:' + response);
+                if (response.status === 401) {
+                    var deferred = $q.defer(),
+                            req = {
+                                config: response.config,
+                                deferred: deferred
+                            };
+                    $rootScope.requests401.push(req);
+                    $rootScope.$broadcast('event:loginRequired');
+                    return deferred.promise;
+                }
+                return $q.reject(response);
+            }
+
+        };
+    });
+
+
+    httpHeaders = $httpProvider.defaults.headers;
+});
