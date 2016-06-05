ee9b52bcc62e76558c742650901abf9e7b1aad4b
i1990jain <i1990jain@gmail.com>
2016-06-05 03:53:27 +0200

    ng-HighStockchart with basic functionalities,home page template added

--- src/main/webapp/resources/js/controller/HistogramController.js
+++ src/main/webapp/resources/js/controller/HistogramController.js
@@ -1,34 +1,124 @@
-app.controller('HistogramController', [ '$scope','$http','$location',	function($scope,$http,$location) {
+app.controller('HistogramController', [ '$scope','$http','$location','$document','$localStorage',	function($scope,$http,$location,$document,$localStorage) {
+
+	$scope.toggleLoading = function () {
+		this.chartConfig.loading = !this.chartConfig.loading
+	}
+	
+	$scope.types = [ {code: 1, name: 'Days'}, {code: 2, name: 'Months'}, {code: 3, name: 'Years'}];
 	
 	
-	 $scope.chartConfig = {
-			 options: {
-	        chart: {
-	            type: 'column'
-	        }
-			 },
-	        title: {
-	            text: 'Water Consumption'
-	        },
-	        xAxis: {
-	            categories: ['Apples', 'Bananas', 'Oranges']
-	        },
-	        yAxis: {
-	            title: {
-	                text: 'Water Consumption [m3]'
-	            }
-	        },
-	        series: [{
-	            name: 'Jane',
-	            data: [1, 6, 4]
-	        }, {
-	            name: 'John',
-	            data: [5, 7, 3]
-	        }]
-	    }
-	
-	
-	
-	
-	
+	$scope.chartConfig = {
+			options: {
+				chart: {
+					type: 'column',
+					alignTicks: false
+				},
+				rangeSelector: {
+					buttons: [{
+	                    type: 'month',
+	                    count: 1,
+	                    text: '1m'
+	                }, {
+	                    type: 'month',
+	                    count: 3,
+	                    text: '3m'
+	                },
+	                {
+	                    type: 'month',
+	                    count: 6,
+	                    text: '6m'
+	                },{
+	                    type: 'all',
+	                    text: 'All'
+	                }],
+	                
+					selected: 0,
+					enabled: true
+				},
+				navigator: {
+					enabled: true
+				}
+			},
+			 yAxis: {
+		            title: {
+		                text: 'Water Consumption [m\u00B3]'
+		            }
+		        },
+			series: [],
+			title: {
+				text: 'Household Consumption'
+			},
+			useHighStocks: true
+	}
+
+	$document.ready(function () {
+		Highcharts.setOptions({
+			global: {
+				useUTC: false
+			}
+		});
+		$scope.toggleLoading();
+
+		var token=$localStorage.currentUser.token;
+		var data = {
+				oid : token
+		};
+
+		$http.post('histogram/', data)
+		.success(function (response) {
+			var array=response.result;
+			var readingData =[];
+
+			for (var i in array) {
+				var reading=[array[i].readingDateTime,array[i].totalConsumptionAdjusted]
+				readingData.push(reading);
+
+			}
+			readingData.sort();
+			
+			$scope.chartConfig.series.push({
+				name: 'Consumption',
+				data:readingData,
+				tooltip: {
+                    valueDecimals: 1,
+                    valueSuffix: ' m\u00B3'
+                },
+				dataGrouping: {
+		            approximation: "average",
+		            enabled: true,
+		            units: [[
+		                'day',
+		                [1]
+		                ]]
+		        
+				}
+			});
+
+			
+
+
+			$scope.toggleLoading();
+		});
+	});
+
+	$scope.chartTypeChange = function () {
+				
+		if($scope.chartType.name==="Days"){
+			$scope.chartConfig.series[0].dataGrouping.units=[['day',[1]]];
+			this.chartConfig.options.rangeSelector.selected=0;
+		}
+		
+		if($scope.chartType.name==="Months"){
+			$scope.chartConfig.series[0].dataGrouping.units=[['month',[1]]];
+			this.chartConfig.options.rangeSelector.selected=2;
+		
+		}
+		
+		if($scope.chartType.name==="Years"){
+			$scope.chartConfig.series[0].dataGrouping.units=[['year',[1]]];
+			this.chartConfig.options.rangeSelector.selected=3;
+		}
+	}
+
+
 }]);
