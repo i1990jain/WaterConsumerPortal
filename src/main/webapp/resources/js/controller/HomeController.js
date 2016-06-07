42e26175da0064c6fc14ef7d341d0d6508ac558e
i1990jain <i1990jain@gmail.com>
2016-06-07 05:11:14 +0200

    UserData Added to Home Page

--- src/main/webapp/resources/js/controller/HomeController.js
+++ src/main/webapp/resources/js/controller/HomeController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('HomeController', [ '$scope','$http','$location','AuthenticationService','$localStorage','$route',	function($scope,$http,$location,AuthenticationService,$localStorage,$route) {
+app.controller('HomeController', [ '$scope','$http','$location','AuthenticationService','$localStorage','$route','$document',	function($scope,$http,$location,AuthenticationService,$localStorage,$route,$document) {
 	var user = this;
 	
 	user.logout = logout;
@@ -12,6 +12,25 @@ app.controller('HomeController', [ '$scope','$http','$location','AuthenticationS
 		
 	      return 'app/histogram';
 	  }
+	
+	$document.ready(function () {
+		
+		var token=$localStorage.currentUser.token;
+		var data = {
+				oid : token
+		};
+		
+		$http.post('userdata/', data)
+		.success(function (response) {
+			$scope.houseHoldId=response.result.houseHoldId;
+			$scope.smartMeterId=response.result.smartMeterId;
+			$scope.buildingId=response.result.buildingId;
+			$scope.consumptionType=response.result.consumptionType;
+			$scope.zipcode=response.result.zipcode;
+			$localStorage.currentUser.zipcode=response.result.zipcode;
+			$scope.country=response.result.country;
+		});
+	});
 	
 	function logout() {
 		console.log("here")
