1f66e16cbe407d495e70db750c5a532d392561a3
i1990jain <i1990jain@gmail.com>
2016-07-12 01:54:58 +0200

    Fixed code to not add metered user to consumer segment table

--- src/main/java/com/wcp/service/RegisterService.java
+++ src/main/java/com/wcp/service/RegisterService.java
@@ -30,8 +30,8 @@ public class RegisterService {
 
 		Session session = sessionFactory.openSession();
 
-		String smartMeterId="" ;
-		int buidlingID=0;
+		String smartMeterId = "";
+		int buidlingID = 0;
 		int householdid = 0;
 		int checked = 0;
 		User user = new User();
@@ -48,7 +48,7 @@ public class RegisterService {
 			System.out.println("First Name :" + registerdata.getFirstName());
 			System.out.println("Last Name: " + registerdata.getLastName());
 			System.out.println("HouseholdID : " + registerdata.getHouseholdID());
-			System.out.println("SmartMeter : "+ registerdata.isSmartmeter());
+			System.out.println("SmartMeter : " + registerdata.isSmartmeter());
 			System.out.println("ZipCode : " + registerdata.getZipCode());
 			System.out.println("Password : " + registerdata.getPassword());
 
@@ -57,34 +57,31 @@ public class RegisterService {
 			System.out.println(householdidcheck);
 
 			Query householdcheckquery = session.createQuery(householdidcheck);
-			householdcheckquery.setParameter("household",registerdata.getHouseholdID());
+			householdcheckquery.setParameter("household", registerdata.getHouseholdID());
 			List householdresult = householdcheckquery.list();
 			System.out.println(householdresult);
 			System.out.println("resultset:" + householdresult);
 			Iterator iterator = householdresult.iterator();
 			while (iterator.hasNext()) {
 				householdid = (Integer) iterator.next();
-				/*		if (householdid == registerdata.getHouseholdID()) {
-						a = 0;
-						break;
-					} else {
-						a = 1;
-					}*/
+				/*
+				 * if (householdid == registerdata.getHouseholdID()) { a = 0;
+				 * break; } else { a = 1; }
+				 */
 			}
-
-
 
 			// if householdID is valid register User
 			if (householdid != 0) {
-				//if the smartMeter is being not being checked.Check for smartMeter and building Id before registration.
+				// if the smartMeter is being not being checked.Check for
+				// smartMeter and building Id before registration.
 
-				if(registerdata.isSmartmeter()==false){
-					//Check for smartmeterId
+				if (!registerdata.isSmartmeter()) {
+					// Check for smartmeterId
 					System.out.println("Check for smartMeterID");
 					String smartMeterCheck = "select smartMeterId from SmartMeter where (oid = (select smartMeter.oid from Household where oid=:householdOid))";
 					System.out.println(smartMeterCheck);
 					Query smartmetercheckquery = session.createQuery(smartMeterCheck);
-					smartmetercheckquery.setParameter("householdOid",householdid);
+					smartmetercheckquery.setParameter("householdOid", householdid);
 					List result1 = smartmetercheckquery.list();
 					System.out.println(result1);
 					System.out.println("resultset:" + result1);
@@ -93,14 +90,14 @@ public class RegisterService {
 						smartMeterId = (String) iterator1.next();
 					}
 					if (!smartMeterId.isEmpty()) {
-						map.put("response","SmartMeterIDFound");
+						map.put("response", "SmartMeterIDFound");
 
-						//Check for buildingID
+						// Check for buildingID
 						System.out.println("Check for BuildingID");
 						String buildingIDCheck = "select building.oid from Household where oid=:householdOid";
 						System.out.println(buildingIDCheck);
 						Query buildingIDCheckQuery = session.createQuery(buildingIDCheck);
-						buildingIDCheckQuery.setParameter("householdOid",householdid);
+						buildingIDCheckQuery.setParameter("householdOid", householdid);
 						List result2 = buildingIDCheckQuery.list();
 						System.out.println(result2);
 						System.out.println("resultset:" + result2);
@@ -109,21 +106,23 @@ public class RegisterService {
 							buidlingID = (Integer) iterator2.next();
 						}
 
-						if (buidlingID==0) {
-							map.put("responsemsg","No buildingIDFound.Please click the Checkbox to Register as Non-Metered User");
+						if (buidlingID == 0) {
+							map.put("responsemsg",
+									"No buildingIDFound.Please click the Checkbox to Register as Non-Metered User");
 							map.put("response", "Unauthorized");
-							checked=0;
-						}else{
-							map.put("response","BuidlingIDFound");
-							checked=1;
-						}	
+							checked = 0;
+						} else {
+							map.put("response", "BuidlingIDFound");
+							checked = 1;
+						}
 						System.out.println(buidlingID);
-					}else{
-						map.put("responsemsg","NoSmartMeterIDFound.Please click the Checkbox to Register as Non-Metered User");
+					} else {
+						map.put("responsemsg",
+								"NoSmartMeterIDFound.Please click the Checkbox to Register as Non-Metered User");
 						map.put("response", "Unauthorized");
-						checked=0;
-					}	
-				}else{
+						checked = 0;
+					}
+				} else {
 					int oid = 0;
 					String selectoid = "SELECT oid FROM User ORDER BY oid DESC";
 					System.out.println(selectoid);
@@ -148,26 +147,26 @@ public class RegisterService {
 					user.setPassword(registerdata.getPassword());
 
 					session.save(user);
-					//Update the NeutralUsertable
+					// Update the NeutralUsertable
 
 					neutraluser.setUser(user);
 					neutraluser.setHousehold(houseHold);
 					session.save(neutraluser);
-					//Update the consumer-segment
+					// Update the consumer-segment
 					consumersegment.setOid(oid);
 					consumersegment.setName(registerdata.getFirstName());
 					consumersegment.setDescription("Non-MeteredUser");
-					session.save(consumersegment);	
+					session.save(consumersegment);
 
 					// Commit the transaction
 					session.getTransaction().commit();
 
 					System.out.println("Insert completed");
-					//	Insert(registerdata);
+					// Insert(registerdata);
 					map.put("response", "Authorized");
 				}
 
-				if(checked==1){
+				if (checked == 1) {
 					int oid = 0;
 					String selectoid = "SELECT oid FROM User ORDER BY oid DESC";
 					System.out.println(selectoid);
@@ -191,25 +190,19 @@ public class RegisterService {
 					user.setPassword(registerdata.getPassword());
 
 					session.save(user);
-					//Update the NeutralUsertable		
+					// Update the NeutralUsertable
 					neutraluser.setUser(user);
 					neutraluser.setHousehold(houseHold);
 					session.save(neutraluser);
-					
-					//Update the consumer-segment
-					consumersegment.setOid(oid);
-					consumersegment.setName(registerdata.getFirstName());
-					consumersegment.setDescription("Non-MeteredUser");
-					session.save(consumersegment);
-					
+
 					// Commit the transaction
 					session.getTransaction().commit();
 
 					System.out.println("Insert completed");
-					//	Insert(registerdata);
+					// Insert(registerdata);
 					map.put("response", "Authorized");
 				}
-			}else {
+			} else {
 				map.put("responsemsg", "No HouseholdId found.Put a Valid Household ID");
 				map.put("response", "Unauthorized");
 				session.flush();
@@ -223,48 +216,36 @@ public class RegisterService {
 		}
 		return map;
 	}
-	/*	void Insert(RegisterData registerdata){
-		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-		Session session1 = sessionFactory.openSession();
-
-		int oid = 0;
-		String selectoid = "SELECT oid FROM User ORDER BY oid DESC";
-		System.out.println(selectoid);
-		Query selectoidquery = session1.createQuery(selectoid);
-		selectoidquery.setMaxResults(1);
-		List result1 = selectoidquery.list();
-		System.out.println(result1);
-		System.out.println("resultset:" + result1);
-		Iterator iterator2 = result1.iterator();
-		while (iterator2.hasNext()) {
-			oid = (Integer) iterator2.next();
-		}
-		oid = oid + 1;
-		System.out.println(oid);
-		User user = new User();
-		NeutralUser neutraluser = new NeutralUser();
-		Household houseHold = new Household();
-		houseHold.setOid(registerdata.getHouseholdID());
-		user.setOid(oid);
-		user.setUsername(registerdata.getUserName());
-		user.setFirstName(registerdata.getFirstName());
-		user.setLastName(registerdata.getLastName());
-		user.setEmail(registerdata.getEmail());
-		user.setPassword(registerdata.getPassword());
-
-		session1.save(user);
-		neutraluser.setUser(user);
-		neutraluser.setHousehold(houseHold);
-		session1.save(neutraluser);
-	//Update the consumer-segment
-					consumersegment.setOid(oid);
-					consumersegment.setName(registerdata.getFirstName());
-					consumersegment.setDescription("Non-MeteredUser");
-					session.save(consumersegment);	
-		// Commit the transaction
-		session1.getTransaction().commit();
-
-		System.out.println("Insert completed");
-	}*/
+	/*
+	 * void Insert(RegisterData registerdata){ SessionFactory sessionFactory =
+	 * HibernateUtil.getSessionFactory();
+	 * 
+	 * Session session1 = sessionFactory.openSession();
+	 * 
+	 * int oid = 0; String selectoid = "SELECT oid FROM User ORDER BY oid DESC";
+	 * System.out.println(selectoid); Query selectoidquery =
+	 * session1.createQuery(selectoid); selectoidquery.setMaxResults(1); List
+	 * result1 = selectoidquery.list(); System.out.println(result1);
+	 * System.out.println("resultset:" + result1); Iterator iterator2 =
+	 * result1.iterator(); while (iterator2.hasNext()) { oid = (Integer)
+	 * iterator2.next(); } oid = oid + 1; System.out.println(oid); User user =
+	 * new User(); NeutralUser neutraluser = new NeutralUser(); Household
+	 * houseHold = new Household();
+	 * houseHold.setOid(registerdata.getHouseholdID()); user.setOid(oid);
+	 * user.setUsername(registerdata.getUserName());
+	 * user.setFirstName(registerdata.getFirstName());
+	 * user.setLastName(registerdata.getLastName());
+	 * user.setEmail(registerdata.getEmail());
+	 * user.setPassword(registerdata.getPassword());
+	 * 
+	 * session1.save(user); neutraluser.setUser(user);
+	 * neutraluser.setHousehold(houseHold); session1.save(neutraluser); //Update
+	 * the consumer-segment consumersegment.setOid(oid);
+	 * consumersegment.setName(registerdata.getFirstName());
+	 * consumersegment.setDescription("Non-MeteredUser");
+	 * session.save(consumersegment); // Commit the transaction
+	 * session1.getTransaction().commit();
+	 * 
+	 * System.out.println("Insert completed"); }
+	 */
 }
